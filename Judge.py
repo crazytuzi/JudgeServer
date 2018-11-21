d47aeb6d738bfc583cff3f61706477b3a41b0bce
crazytuzi <957824770@qq.com>
2018-11-21 11:28:06 +0800

    Judger RES

--- Judge.py
+++ Judge.py
@@ -19,7 +19,7 @@ class Judge(object):
 
     def __init__(self, data):
         self.data = data
-        self.problem_dir = Utlis.problem_dir(data['problem'])
+        self.problem_dir = Utlis.problem_dir(self.data['problem'])
         self.file_len = len([name for name in os.listdir(
             self.problem_dir) if os.path.isfile(os.path.join(self.problem_dir, name))])
         self.file_len //= 2
@@ -36,9 +36,14 @@ class Judge(object):
             'memory_limit': self.data['memory_cost'],
             'compile option': ['-O2', '-lm', '-DONLINE_JUDGE']
         }
+        self.RES = {}
         self.JudgeAll()
+        self.response()
 
     def JudgeAll(self):
+        max_use_time = -1
+        max_use_memory = -1
+        res = {}
         for i in range(self.file_len):
             in_file = self.problem_dir + str(i) + self.file_extensions[0]
             ans_file = self.problem_dir + str(i) + self.file_extensions[1]
@@ -48,12 +53,22 @@ class Judge(object):
             self.CFG['in_file'] = in_file
             self.CFG['out_file'] = out_file
             self.CFG['ans_file'] = ans_file
-            RES = self.JudgeOne(self.CFG)
-            print(RES)
+            res = self.JudgeOne(self.CFG)
+            if res['status'] != 1:
+                self.RES = res
+                return
+            elif res['use_time'] > max_use_time:
+                max_use_time = res['use_time']
+                max_use_memory = res['use_memory']
+            elif res['use_time'] == max_use_time and res['use_memory'] > max_use_memory:
+                max_use_memory = res['use_memory']
+        res['use_time'] = max_use_time
+        res['use_memory'] = max_use_memory
+        self.RES = res
 
     def JudgeOne(self, config):
-        RES = Judger.run(config)
-        return RES
+        res = Judger.run(config)
+        return res
 
     def writefile(self, name, content=''):
         with open(name, 'w') as f:
@@ -61,10 +76,12 @@ class Judge(object):
             f.close()
 
     def response(self):
-        url_tmp = self.url() + str(self.data["id"]) + "/"
-        jdata = {
-            "id": self.data["id"],
-            "result": 5,
+        url = Utlis.submission_url(self.data['id'])
+        data = {
+            "id": self.data['id'],
+            "result": self.RES['status'],
+            "time_cost": self.RES['use_time'],
+            "memory_cost": self.RES['use_memory'],
             "token": self.data["token"]
         }
-        req = requests.put(url_tmp, json=jdata)
+        requests.put(url, json=data)
--- Utlis.py
+++ Utlis.py
@@ -12,5 +12,7 @@ def problem_dir(problem):
 def answer_dir():
     return path[1][1]
 
+
 def submission_url(submission):
-    print(url)
+    return url[0][1] + ':' + url[1][1] + \
+        '/' + url[2][1] + str(submission) + '/'
